ddd14428012d51934364132af0bef6eab493521b
yuansamuelandres <yuandeslytherinandres@gmail.com>
2025-03-11 13:46:22 +0200

    Added the (26th-29th) sets of Python code notes

--- tutorial.py
+++ tutorial.py
@@ -1,30 +1,73 @@
 # In the name of Allah
 
-''' #024 & #025: Tuples & Methods '''
-# Tuple items are enclosed in parentheses & can be removed.
-# Tuple items are ordered & indexed.
-# Tuples are immutable.
-# Tuple items aren't unique.
-# Tuples can have different data types.
-# Strings & lists operators are used in tuples.
+''' #026, #027, #028, #029: Set & Methods '''
+# Set items are enclosed in curly braces.
+# Set items aren't ordered nor indexed.
+# Set indexing & slicing can't be done.
+# Set only has immutable data types (not unhashable types)
+# Set items are unique.
 
-# Tuple with one element ("",) :
-mine = "Yuan",
-print(type(mine))   # 'tuple'
+mine = {"Yuan", 5, True}
+print(mine) # {True, 'Yuan', 5}
 
+# Union()
+his = {"N", 23, False, 5}
+print(his.union(mine))  # {False, True, 'Yuan', 5, 23, 'N'}
+theirs = {'G'}
+print(his.union(mine, theirs))  # {False, True, 'G', 5, 23, 'Yuan', 'N'}
 
-# Tuple Concatenation
-a = 1, 2, "C", 3
-b = 4, 5
-c = a + b
-print(c)    # (1, 2, 3, 4, 5)
+# Adding
+mine.add(22)
+print(mine) # {True, 5, 22, 'Yuan'}
 
-# Indexing
-print("The position of index is: {:d}".format(c.index(3)))  # The position of index is: 2
-print(f"The position of index is: {c.index(3)}")    # The position of index is: 2
+# Copying (Shallow Copy)
 
-# Destructing
-x, y, _, z = a
-print(x)    # 1
-print(y)    # 2
-print(z)    # 3
+# Removing (Throws an error if element not found)
+his.remove(False)
+# his.remove(True)    # Error
+
+# Discarding (Doesn't throw an error if element not found)
+his.discard(False)
+his.discard(True)   # No error
+
+# Updating
+theirs.update(his)
+print(theirs)   # {'N', 'G', 23}
+
+# difference()
+print(mine.difference(his)) # {'Yuan', True, 5, 22}
+
+# difference_update() (Update the original Set with the difference)
+his.difference_update(mine)
+print(his)  # {23, 'N'}
+
+# intersection()
+print(his.intersection(mine))   # set() 'No intersection'
+
+# intersection_update()
+his.intersection_update(mine)
+print(his)
+
+# symmetric_difference()
+print(theirs)   # {5, 23, 'G', 'N'}
+print(mine) # {True, 'Yuan', 5, 22}
+print(theirs.symmetric_difference(mine))   # {True, 'Yuan', 'G', 'N', 22, 23}
+
+# symmetric_difference_update()
+print(theirs)   # {5, 23, 'G', 'N'}
+print(mine) # {True, 'Yuan', 5, 22}
+theirs.symmetric_difference_update(mine)
+print(theirs)   # {True, 'N', 'G', 'Yuan', 22, 23}
+
+# issuperset()
+a = {1, 2, 3}
+b = {1, 2}
+print(a.issuperset(b))  # True
+
+# issubset()
+a = {1, 2, 3}
+b = {1, 2}
+print(a.issubset(b))    # False
+
+# isdisjoint()
+print(b.isdisjoint(a))  # False

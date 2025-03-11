127f9cf50a42e755e14c517f3bf96ffd9787703f
yuansamuelandres <yuandeslytherinandres@gmail.com>
2025-03-11 12:32:18 +0200

    Added the twentieth-third set of Python code notes

--- tutorial.py
+++ tutorial.py
@@ -1,12 +1,25 @@
 # In the name of Allah
 
-''' #021: Lists '''
-myList = ["one", 1, True, 100.5]
+''' #023: Lists Methods II '''
+# Clearing
+a = [1, 2, 5]
+a.clear()
 
-# Slicing
-print(myList[1:4]) # [1, True, 100.5]
-print(myList[::2]) # ['one', True]
+# Copying
+b = [1, 2, 3, 4]
+c = b.copy()    # Shallow Copy
 
-# Mutability (Editing not replacing)
-myList[1:3] = "one", False
-print(myList) 
+
+# Counting
+d = [1, 2, 1, 33, 2, 5, 1]
+print(d.count(1))   # 3
+
+# Indexing
+print(d.index(2))   # 1
+
+# Inserting (Before the index)
+d.insert(0, "Sam")
+print(d)    # ['Sam', 1, 2, 1, 33, 2, 5, 1]
+
+# Popping
+print(d.pop(-3)) # 2 (The value deleted at index -3)

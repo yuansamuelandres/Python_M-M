52712d4d91acc7406fa4ad502dffd3c8ff76e644
yuansamuelandres <yuandeslytherinandres@gmail.com>
2025-03-11 12:19:39 +0200

    Edited the twentieth-first set of Python code

--- tutorial.py
+++ tutorial.py
@@ -1,34 +1,12 @@
 # In the name of Allah
 
-''' #022: List Methods I '''
-# Appending
-myFriends = ["Ahmad", "Paul", False]
-myFriends.append("Mazen")
-print(myFriends) # ['Ahmad', 'Paul', False, 'Mazen']
+''' #021: List Methods II '''
+myList = ["one", 1, True, 100.5]
 
-# Extending
-nums = ["one", "two"]
-no = [1, 2, 3]
-nums.append(no)
-print(nums) # ['one', 'two', [1, 2, 3]]
-nums.extend(no)
-print(nums) # ['one', 'two', [1, 2, 3], 1, 2, 3]
+# Slicing
+print(myList[1:4]) # [1, True, 100.5]
+print(myList[::2]) # ['one', True]
 
-# Removing (Only the first encountered element)
-myFriends.remove("Paul")
-
-# Sorting (Numbers Only || Strings Only)
-y = [1, 998, -19, 0]
-y.sort() # Default: reverse=False 
-print(y) # [-19, 0, 1, 998]
-y.sort(reverse=True)
-print(y) # [998, 1, 0, -19]
-
-x = ['A', 'Y', 'M']
-x.sort()
-print(x) # ['A', 'M', 'Y']
-
-# Reversing (Only reversing the list)
-z = [ 10, 1, "Zero", 556, 7657, "Osama"]
-z.reverse()
-print(z) # ['Osama', 7657, 556, 'Zero', 1, 10]
+# Mutability (Editing not replacing)
+myList[1:3] = "one", False
+print(myList) 
